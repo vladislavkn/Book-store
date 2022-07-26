025c9332bd08b909906eddf7a317bf48e55dcf73
Vladislav Knyshov <inova136@gmail.com>
2022-07-26 22:16:34 +0300

    feat: finish styling at sign-up page

--- src/views/Sign-up.vue
+++ src/views/Sign-up.vue
@@ -1,12 +1,42 @@
 <template>
-  <main>
-    <form>
-      <input type="email" />
-      <input type="password" />
-      <input type="password" />
-      <button type="submit">Complete</button>
+  <EmptyLayout class="flex-col">
+    <h1 class="text-3xl font-bold text-gray-800 max-w-xs text-center mb-2">
+      Book.Store Registration
+    </h1>
+    <form class="max-w-xs w-full mb-4 space-y-3" @submit.prevent>
+      <input class="InputText" type="email" placeholder="Email" />
+      <input class="InputText" type="password" placeholder="Password" />
+      <input class="InputText" type="password" placeholder="Repeat password" />
+      <div class="flex justify-end">
+        <button class="Button Button__primary" type="submit">Complete</button>
+      </div>
     </form>
-    <button>Log in with Google</button>
-    <button>Log in via email (passwordless)</button>
-  </main>
+    <div class="text-center mb-4 max-w-xs w-full relative">
+      <div class="Divider" />
+      <span class="text-gray-600 px-2 bg-white z-10 relative">Or</span>
+    </div>
+    <div class="max-w-xs w-full space-y-3">
+      <button class="Button Button_secondary w-full">
+        Continue with Google
+      </button>
+      <button class="Button Button_secondary w-full">
+        Continue with email (passwordless)
+      </button>
+    </div>
+  </EmptyLayout>
 </template>
+
+<script>
+import EmptyLayout from "../components/EmptyLayout.vue";
+
+export default {
+  components: { EmptyLayout },
+};
+</script>
+
+<style>
+.Divider {
+  @apply h-px left-0 right-0 bg-gray-300 z-0 absolute;
+  top: calc(50% - 0.5px);
+}
+</style>
