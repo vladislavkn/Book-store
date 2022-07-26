83d0d41b7cbd4833b4c670f8048e0b347935a283
Vladislav Knyshov <inova136@gmail.com>
2022-07-26 21:54:17 +0300

    feat: add layouts components

---
+++ src/components/Layout.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <Navbar />
+    <div class="navbar-placeholder" />
+    <div class="router-view w-screen">
+      <slot />
+    </div>
+    <ActionSheet />
+  </div>
+</template>
+
+<script>
+import ActionSheet from "./ActionSheet.vue";
+import Navbar from "./Navbar.vue";
+
+export default {
+  components: { ActionSheet, Navbar },
+};
+</script>
+
+<style>
+.navbar-placeholder {
+  height: 108px;
+}
+
+.router-view {
+  max-height: calc(100% - 108px);
+  overflow-y: auto;
+}
+
+@media (min-width: 1024px) {
+  .navbar-placeholder {
+    height: 56px;
+  }
+  .router-view {
+    max-height: calc(100% - 56px);
+  }
+}
+</style>
