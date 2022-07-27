3cf3e8a2682ea817f3109adefc190634082b719a
Vladislav Knyshov <inova136@gmail.com>
2022-07-27 21:21:02 +0300

    feat: add sign-in view

---
+++ src/views/Sign-in.vue
@@ -0,0 +1,28 @@
+<template>
+  <EmptyLayout class="flex-col">
+    <h1 class="text-3xl font-bold text-gray-800 max-w-xs text-center mb-4">
+      Login
+    </h1>
+    <AuthForm
+      :showPasswordRepeat="false"
+      :isLoading="isLoading"
+      :error="error"
+      linkRoute="/sign-up"
+      linkText="Register instead"
+      @validationError="error = $event"
+      @submit="submit"
+      @loginWithGoogle="loginWithGoogle"
+    />
+  </EmptyLayout>
+</template>
+
+<script>
+import EmptyLayout from "../components/EmptyLayout.vue";
+import AuthForm from "../components/AuthForm.vue";
+import authMixin from "../../mixins/authMixin";
+
+export default {
+  components: { EmptyLayout, AuthForm },
+  mixins: [authMixin],
+};
+</script>
