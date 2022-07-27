359f81738db7e33f2a85512e61302c305b4b0628
Vladislav Knyshov <inova136@gmail.com>
2022-07-27 21:28:48 +0300

    fix: submit function in authMixin gets params from args

--- mixins/authMixin.js
+++ mixins/authMixin.js
@@ -4,13 +4,10 @@ export default (authEventType) => ({
     error: "",
   }),
   methods: {
-    submit() {
+    submit({ email, password }) {
       this.isLoading = true;
       this.$store
-        .dispatch(authEventType, {
-          email: this.email,
-          password: this.password,
-        })
+        .dispatch(authEventType, { email, password })
         .then(() => this.$router.push("/"))
         .catch((error) => (this.error = error.message))
         .finally(() => (this.isLoading = false));
