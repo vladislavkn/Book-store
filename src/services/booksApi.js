7d719d17ed04474bb4ade5154144a14d144395dd
Vladislav Knyshov <inova136@gmail.com>
2022-07-29 18:35:59 +0300

    featt: add books & authors search api

---
+++ src/services/booksApi.js
@@ -0,0 +1,21 @@
+import { Book } from "../store/books";
+
+const BASE_URL = "http://openlibrary.org";
+
+export const searchBookByTitle = (title) =>
+  fetch(`${BASE_URL}/search.json?title=${title}&limit=3`)
+    .then((res) => res.json())
+    .then((res) =>
+      res.docs.map((doc) => {
+        return new Book({
+          title: doc.title,
+          id: doc.key,
+          author: doc.author_name.join(", "),
+        });
+      })
+    );
+
+export const searchAuthorByName = (name) =>
+  fetch(`${BASE_URL}/search/authors.json?q=${name}&limit=3`)
+    .then((res) => res.json())
+    .then((res) => res.docs.map((doc) => doc.name));
