f096d4b1e79fd11ea72c210fe64ab6f9937f6e9a
Vladislav Knyshov <inova136@gmail.com>
2022-07-29 17:43:36 +0300

    feat: connect vue-tost-notification

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import i18n from "./i18n";
+import VueToast from "vue-toast-notification";
+import "vue-toast-notification/dist/theme-sugar.css";
 import "./assets/css/tailwind.css";
+
 Vue.config.productionTip = false;
+Vue.use(VueToast);
 
 new Vue({
   router,
