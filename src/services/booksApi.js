6d64394892162485e641f4244a1249bce46c4361
Vladislav Knyshov <inova136@gmail.com>
2022-07-29 20:32:40 +0300

    chore: beautify search book api

--- src/services/booksApi.js
+++ src/services/booksApi.js
@@ -5,15 +5,15 @@ const BASE_URL = "http://openlibrary.org";
 export const searchBookByTitle = (title) =>
   fetch(`${BASE_URL}/search.json?title=${title}&limit=3`)
     .then((res) => res.json())
-    .then((res) =>
-      res.docs.map((doc) => {
+    .then((res) => {
+      return res.docs.map((doc) => {
         return new Book({
           title: doc.title,
           id: doc.key,
           author: doc.author_name.join(", "),
         });
-      })
-    );
+      });
+    });
 
 export const searchAuthorByName = (name) =>
   fetch(`${BASE_URL}/search/authors.json?q=${name}&limit=3`)
