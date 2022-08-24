ddb1e37f5fe7dd0d8124ef0278e05e070b62b580
Vladislav Knyshov <inova136@gmail.com>
2022-08-24 11:02:18 +0300

    feat: remove i18n

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import i18n from "./i18n";
 import VueToast from "vue-toast-notification";
 import "vue-toast-notification/dist/theme-sugar.css";
 import "./assets/css/tailwind.css";
@@ -16,9 +15,5 @@ Vue.use(VueToast, {
 new Vue({
   router,
   store,
-  i18n,
-  mounted() {
-    this.$i18n.locale = this.$store.getters.getLocale;
-  },
   render: (h) => h(App),
 }).$mount("#app");
