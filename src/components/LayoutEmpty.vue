045fa87ad2aab610916558f795ab2aad1f43503b
Vladislav Knyshov <inova136@gmail.com>
2022-08-24 11:03:22 +0300

    chore: remove old components & pages

--- src/components/LayoutEmpty.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="flex min-w-full min-h-full items-center justify-center p-2">
-    <slot />
-  </div>
-</template>
--- src/views/EditBook.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="container mx-auto px-2 py-2">
-    <p>Page not implemented yet</p>
-    <router-link to="/" class="text-blue-600">Go home</router-link>
-  </div>
-</template>
-<script>
-export default {};
-</script>
